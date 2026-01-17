cb75167d4e91eae894fe8f0230fc772301f6f243
Santiago Nungaray <93005954+snungaray729@users.noreply.github.com>
2026-01-17 09:04:24 -0800

    Add files via upload

---
+++ basic-html-website/contact-page/contact.js
@@ -0,0 +1,7 @@
+const form = document.getElementById('form')
+const fname = document.getElementById('fname')
+const lname = document.getElementById('lname')
+const email = document.getElementById('email')
+const message = document.getElementById('message')
+
+form.addEventListener("submit", storeMessage)
